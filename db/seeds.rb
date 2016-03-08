db39976387b4287f02094e509646d587662e5d9c
Joseph DePompeis <josephdepompeis@gmail.com>
2016-03-08 02:18:20 -0500

    fixed image for seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-    coursex = Course.create([{name: "Cous Cous", image: "whatever.jpg"}, {name: "Fruit Loops", image: "froot.jpeg"}, {name: "Meatloaf", image: "meat.jpg"}])
+    coursex = Course.create([{name: "Cous Cous", image: "whatever.jpg"}, {name: "Fruit Loops", image: "fruit.jpg"}, {name: "Meatloaf", image: "meat.jpg"}])
     frootloops = Dish.create([{name: "Milk",price: 2, description: "Ice cold", course_id: 2}, {name: "Cereal",price: 2, description: "Fruity", course_id: 2}  ])
     meaty = Dish.create([{name: "Meat",price: 6, description: "Loafy", course_id: 3}, {name: "Sauce",price: 6, description: "Gooey", course_id: 3}  ])
     meaty = Dish.create([{name: "Veggies",price: 6, description: "Beans n' things", course_id: 1}, {name: "Ingrediant X",price: 5, description: "Undisclosed", course_id: 1}])
